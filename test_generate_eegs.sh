aa409b56fa6f92a87e4f5192bf29b74e2aeee15c
acumpelik <acumpelik@gmail.com>
2024-04-16 13:50:33 +0200

    test gen eegs works now, generates dat files with leading zero when needed

--- test_generate_eegs.sh
+++ test_generate_eegs.sh
@@ -5,10 +5,20 @@
 
 # Set parameters
 animal=$1
-dir="/adata_pool/processing"
 day=$2
+dir="/adata_pool/preprocessing"
 
 cd $dir/$animal/$day
 
 num_dats=$(find *.dat | wc -w)
 echo "The number of dats in $day is $num_dats"
+
+
+for i in $(seq 1 $num_dats)
+do
+	if [ $i -lt 10 ]; then
+		echo "$animal-$day"_0"$i"
+	else
+		echo "$animal-$day"_"$i"
+	fi
+done
