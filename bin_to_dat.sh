79f75e69bf785a4e45f4e1ba7f70736b5247dd4f
acumpelik <acumpelik@gmail.com>
2024-04-16 16:46:12 +0200

    made bin_to_dat.sh work for any repository it's in

--- bin_to_dat.sh
+++ bin_to_dat.sh
@@ -2,12 +2,19 @@
 # 2024-03-31
 # Script to convert several bin files to dat files in the current directory.
 
-animal="JC315"
-day="20240330"
-numbin=6 #number of bin files
+# Find animal and day from path
+animal=$(pwd | rev | cut -c 10-14 | rev)
+day=$(pwd | rev | cut -c 1-8 | rev)
+
+# Count bin files
+numbin=$(find *.bin | wc -w)
 
 basename=$animal-$day
 
-for i in `seq 1 $numbin`; do
-	Axona2dat3_15_128 $basename"_0"$i".bin"
+for i in $(seq 1 $numbin); do
+	if [ $i -lt 10 ]; then
+		Axona2dat3_15_128 $basename"_0"$i".bin"
+	else	
+		Axona2dat3_15_128 $basename"_"$i".bin"
+	fi
 done 
